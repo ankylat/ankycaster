1057fce68a03bc2385db84b25ce326cea0229f52
jp <jp@jps-MacBook-Air.local>
2023-12-05 08:26:58 -0300

    Sojourn 2: Ready, set, GO

---
+++ README.md
@@ -0,0 +1,4 @@
+## Welcome to Ankycaster    
+
+This is a unique system built on top of farcaster, that uses the frontend of Anky to come up with a new way of communicating with each other (and ourselves!).
+
--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,14 +1,24 @@
-import { StyleSheet } from 'react-native';
-
+import { StyleSheet, View, Image } from 'react-native';
+import { StatusBar } from 'expo-status-bar';
 import EditScreenInfo from '../../components/EditScreenInfo';
-import { Text, View } from '../../components/Themed';
+
+import { Text } from '../../components/Themed';
+import ImageViewer from '../../components/ImageViewer';
+import Button from '../../components/Button';
+
+const PlaceholderImage = require('../../assets/images/1412.png');
 
 export default function TabOneScreen() {
   return (
     <View style={styles.container}>
-      <Text style={styles.title}>Tab One</Text>
-      <View style={styles.separator} lightColor="#eee" darkColor="rgba(255,255,255,0.1)" />
-      <EditScreenInfo path="app/(tabs)/index.tsx" />
+      <View style={styles.imageContainer}>
+        <ImageViewer placeholderImageSource={PlaceholderImage} />
+      </View>
+      <View style={styles.footerContainer}>
+        <Button label="Choose a photo" />
+        <Button label="Use this photo" />
+      </View>
+      <StatusBar style="auto" />
     </View>
   );
 }
@@ -16,16 +26,31 @@ export default function TabOneScreen() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
+    backgroundColor: '#25292e',
     alignItems: 'center',
     justifyContent: 'center',
   },
   title: {
     fontSize: 20,
     fontWeight: 'bold',
+    color: '#581292'
   },
   separator: {
     marginVertical: 30,
     height: 1,
     width: '80%',
+  },
+  imageContainer: {
+    flex: 1,
+    paddingTop: 58,
+  },
+  image: {
+    width: 320,
+    height: 440,
+    borderRadius: 18,
+  },
+  footerContainer: {
+    flex: 1 / 3,
+    alignItems: 'center',
   },
 });
